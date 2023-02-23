0b97f1c6ebf099f98845ff50f087b929dbf742d7
Sumit5151 <sumitshitole5151@gmail.com>
2023-02-23 08:31:54 +0530

    Count vowels in the string

--- Program.cs
+++ Program.cs
@@ -7,24 +7,61 @@ public class Program
 
     public static void Main()
     {
-        string FirstName, MiddleName, LastName, FullName;
+        // string FirstName, MiddleName, LastName, FullName;
 
-        Console.WriteLine("Please enter First Name");
-        FirstName = Console.ReadLine();
+        // Console.WriteLine("Please enter First Name");
+        // FirstName = Console.ReadLine();
 
-        Console.WriteLine("Please enter Middle Name");
-        MiddleName = Console.ReadLine();
-
-
-        Console.WriteLine("Please enter Last Name");
-        LastName = Console.ReadLine();
+        // Console.WriteLine("Please enter Middle Name");
+        // MiddleName = Console.ReadLine();
 
 
-        FullName = FirstName +" "+ MiddleName +" "+ LastName;
+        // Console.WriteLine("Please enter Last Name");
+        // LastName = Console.ReadLine();
 
-       Console.WriteLine(FullName);
+
+        // FullName = FirstName +" "+ MiddleName +" "+ LastName;
+
+        //Console.WriteLine(FullName);
 
 
+
+
+
+
+        //Calculate lenght of the string
+
+        //Console.WriteLine("Please enter any string");
+        //  var str= Console.ReadLine(); //Welcome
+
+        //int counter = 0;
+
+        //foreach (var item in str)
+        //{
+        //    counter++;
+        //}
+
+
+        //Console.WriteLine("Lengh of the string = "+counter);
+
+
+        //caluculate the vowels from the string
+
+        Console.WriteLine("Please enter any string");
+        var str = Console.ReadLine();   //Welcome
+
+        int VowelsCounter = 0;
+        foreach (var item in str)
+        {
+
+            if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u'||
+                item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+            {
+                VowelsCounter++;
+            }
+        }
+
+        Console.WriteLine("Number of vowels in the string = "+ VowelsCounter);
     }
 
 }
